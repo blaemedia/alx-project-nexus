59528452e8ab645346030c4a35c8650c7795b3f2
Emmanuel <blaemedia26@gmail.com>
2026-01-30 05:42:30 +0100

    fetching category API

--- blaemart_fronted/app/page.tsx
+++ blaemart_fronted/app/page.tsx
@@ -1,19 +1,26 @@
 // import Image from "next/image";
+import {useState,useEffect} from 'react'
 import Hero from "@/components/Hero/Hero";
-import CategoryCard from "@/components/Cards/CategoryCard"
+// import CategoryCard from "@/components/Cards/CategoryCard"
 import NavBar from "@/components/NavBar/NavBar";
+import React from "react";
 export default function Home() {
+
+  const [DataStr,setDataStr]=useState('')
+
+  useEffect(
+    fetch()
+  )
   return (
     <>
 
      
           <NavBar/>
           <Hero/>
+
           <div className='p-10 grid grid-rows-1 grid-flow-col gap-6 justify-center'>
-            <CategoryCard/>
-            <CategoryCard/>
-            <CategoryCard/>
-            <CategoryCard/>
+            {/* <CategoryCard/> */}
+           
         </div> 
      
     </>
--- blaemart_fronted/components/Cards/CategoryCard.tsx
+++ blaemart_fronted/components/Cards/CategoryCard.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import Image from 'next/image'
-export default function CategoryCard() {
+
+ interface CategoryCardProps {
+  image?: string; // optional with default
+  title?: string; // optional with default
+}
+
+export default function CategoryCard(
+  {
+    image="/images/default.png",
+    title="Default Title"
+  }:
+  
+  CategoryCardProps): React.ReactElement {
+  
   return (
     <div className="w-76 h-64 bg-gray-200 flex flex-col 
     rounded-xl shadow-2xl hover: resize">
   {/* Image container */}
   <div className="relative w-68 mx-auto mt-3 rounded-md bg-gray-100 h-100">
     <Image
-      src="/images/Fashion.jpg"
-      alt="Fashion"
+      src={image}
+      alt={title}
       fill
       className="object-cover scale-100"
       priority
@@ -17,7 +30,7 @@ export default function CategoryCard() {
 
   {/* Text below */}
   <p className="text-2xl font-extrabold text-gray-400 p-2">
-    Fashion
+    {title}
   </p>
 </div>
 
