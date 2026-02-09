a5e36c542bc883d9c83a9b4f95574571970eebde
Emmanuel <blaemedia26@gmail.com>
2026-02-09 12:08:13 +0100

    diagonising the deployment problem

--- backend/BlaeMart/wsgi.py
+++ backend/BlaeMart/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.BlaeMart.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BlaeMart.settings')
 
 application = get_wsgi_application()
